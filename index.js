b9a953a1dfac9d25f693a4c49bd41e28c16fedee
jerryblueberry <93675405+jerryblueberry@users.noreply.github.com>
2024-09-09 20:15:52 +0545

    Cron Fixed

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const cors = require('cors');
 //  Test for cron jobs
 const cron = require('node-cron');
 
-cron.schedule('* * * * * ',() => {
-  console.log("Running Cron Job");
-})
+
 
 
 
@@ -24,8 +22,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Welcome to my Server');
+  // res.send('Welcome to my Server');
+  res.status(200).json('Welcome to my Server');
 });
+cron.schedule('* * * * * ',async() => {
+  try {
+    const response = await axios.get('https://futsalbackend.vercel.app');
+    console.log("Cron Job Executed",response.data);
+  } catch (error) {
+    console.error("Error in Cron Jobs");
+  }
+
+ 
+})
+
 
 app.use('/futsal', Add);
 
