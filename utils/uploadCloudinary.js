ca4f08b9389bd81b752fc24fd48848fce14e9a0e
jerryblueberry <93675405+jerryblueberry@users.noreply.github.com>
2024-09-19 11:18:55 +0545

    Newer Update

--- utils/uploadCloudinary.js
+++ utils/uploadCloudinary.js
@@ -1,26 +1,29 @@
 const cloudinary = require('../config/cloudinaryConfig');
+const streamifier = require('streamifier');
 
+// Optimized function for faster Cloudinary upload
+const uploadCloudinary = (buffer) => {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      {
+        folder: 'user_profiles',
+        transformation: [
+          { width: 100, height: 100, crop: 'scale' },
+          { quality: 'auto' }
+        ],
+      },
+      (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      }
+    );
 
-const uploadCloudinary  = (buffer) => {
-    return new Promise((resolve,reject) => {
-        const stream = cloudinary.uploader.upload_stream(
-            {
-                folder:'user_profiles',
-                transformation:[
-                    {width:600,crop:'scale'},
-                    {quality:'auto'}
-                ],
-            },
-            (error,result) => {
-                if(error){
-                    reject(error)
-                }else{
-                    resolve(result)
-                }
-            }
-        )
-        stream.end(buffer);
-    })
-}
+    // Convert buffer to a stream and pipe it to Cloudinary
+    streamifier.createReadStream(buffer).pipe(stream);
+  });
+};
 
 module.exports = uploadCloudinary;
